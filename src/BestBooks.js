30e082548c69c83d42bd77d6ce549e57d473178d
RawanAlakhras <ralakhras36@gmail.com>
2021-06-21 23:34:46 +0300

    add lab-12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import Carousel from 'react-bootstrap/Carousel'
 import './BestBooks.css';
-
+import axios from 'axios';
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookData: [],
+      userData: [],
+    }
+  }
+  componentDidMount = async () => {
+    const heroku = process.env.REACT_APP_SERVER;
+    const bookURL = `${heroku}/Book`;
+    const userURL = `${heroku}/User`;
+
+    let bookRes = await axios.get(bookURL);
+    let userRes = await axios.get(userURL);
+
+    this.setState({
+      bookData: bookRes.data,
+      userData: userRes.data,
+    });
+  }
   render() {
-    return(
-     <div className='container h-85'>
-        <Jumbotron>
-        <h1 className='text-warning text-center'>My Favorite Books</h1>
-        <p className='text-center'>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
-     </div>
+
+    return (
+
+      <div className='container h-85'>
+        {console.log(this.state.bookData.length)}
+        {
+
+          this.state.bookData.length > 0 &&
+          <Carousel>
+            {this.state.bookData.map((ele, inx) => {
+              return (
+                <Carousel.Item key={inx.toString()}>
+                  <img
+                    className="w-100"
+                    src="holder.js/300x200?bg=2a2025"
+                    alt="First slide"
+                  />
+                  {console.log(ele.name)}
+                  <Carousel.Caption>
+                    <h3>name of the book : {ele.name}</h3>
+                    <p>description : {ele.description}</p>
+                    <p>status: {ele.status}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )
+            })
+            }
+          </Carousel>
+
+        }
+
+      </div>
     )
   }
 }
